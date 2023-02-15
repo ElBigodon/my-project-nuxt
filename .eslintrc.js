09e89ec923c9ca54f9acb34946d2153060087248
ElBigodon <alexandremuassab@gmail.com>
2023-02-15 09:53:50 -0300

    configurações e formatações do eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,33 @@
-export default {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    "eslint:recommended",
-    "plugin:vue/vue3-essential",
-    "plugin:@typescript-eslint/recommended",
-  ],
-  overrides: [],
-  parser: "vue-eslint-parser",
-  plugins: ["vue", "@typescript-eslint"],
-  rules: {
-    indent: ["error", "tab"],
-    "linebreak-style": ["error", "windows"],
-    quotes: ["error", "double"],
-    semi: ["error", "always"],
-    "block-spacing": ["error", "always"],
-  },
+// eslint-disable-next-line no-undef
+module.exports = {
+	root: true,
+	env: {
+		browser: true,
+		es2022: true,
+	},
+	extends: [
+		"eslint:recommended",
+		"plugin:vue/vue3-recommended",
+		"plugin:@typescript-eslint/recommended",
+		"plugin:nuxt/recommended"
+	],
+	overrides: [],
+	parser: "vue-eslint-parser",
+	plugins: ["vue", "@typescript-eslint"],
+	rules: {
+		"no-undef": "off",
+		indent: ["error", "tab"],
+		"linebreak-style": ["error", "windows"],
+		quotes: ["error", "double"],
+		semi: ["error", "always"],
+		"block-spacing": ["error", "always"],
+		"vue/multi-word-component-names": [
+			"error", 
+			{
+				"ignores": ["index", "default"]
+			}
+		],
+		"vue/no-undef-properties": ["error"]
+	},
+	ignorePatterns: ["**.d.ts", "plugins/**/*.{js,ts}"]
 };
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,49 +4,48 @@ import { NaiveUiResolver } from "unplugin-vue-components/resolvers";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
-  modules: ["@vueuse/nuxt", "@vueuse/motion/nuxt", "@nuxtjs/tailwindcss"],
-  typescript: {
-    shim: false,
-  },
-  experimental: {
-    reactivityTransform: true
-  },
-  build: {
-    transpile:
-      process.env.NODE_ENV === "production"
-        ? [
-            "naive-ui",
-            "vueuc",
-            "@css-render/vue3-ssr",
-            "@juggle/resize-observer",
-          ]
-        : ["@juggle/resize-observer"],
-  },
-  vite: {
-    optimizeDeps: {
-      include:
-        process.env.NODE_ENV === "development"
-          ? ["naive-ui", "vueuc", "date-fns-tz/esm/formatInTimeZone"]
-          : [],
-    },
-    plugins: [
-      Components({
-        resolvers: [NaiveUiResolver()],
-      }),
-    ],
-  },
-  imports: {
-    presets: [
-      {
-        from: "naive-ui",
-        imports: [
-          "useMessage", 
-          "useNotification", 
-          "useDialog", 
-          "useLoadingBar"
-        ],
-      },
-    ],
-  },
-
+	modules: ["@vueuse/nuxt", "@vueuse/motion/nuxt", "@nuxtjs/tailwindcss"],
+	typescript: {
+		shim: false,
+	},
+	experimental: {
+		reactivityTransform: true,
+	},
+	build: {
+		transpile:
+			process.env.NODE_ENV === "production"
+				? [
+					"naive-ui",
+					"vueuc",
+					"@css-render/vue3-ssr",
+					"@juggle/resize-observer",
+				]
+				: ["@juggle/resize-observer"],
+	},
+	vite: {
+		optimizeDeps: {
+			include:
+				process.env.NODE_ENV === "development"
+					? ["naive-ui", "vueuc", "date-fns-tz/esm/formatInTimeZone"]
+					: [],
+		},
+		plugins: [
+			Components({
+				resolvers: [NaiveUiResolver()],
+			}),
+		],
+	},
+	imports: {
+		presets: [
+			{
+				from: "naive-ui",
+				imports: [
+					"useMessage",
+					"useNotification",
+					"useDialog",
+					"useLoadingBar",
+				],
+			},
+		],
+	},
 });
