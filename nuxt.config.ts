16f9113ddb12093d1840b1e36adcee7206b85fc8
ElBigodon <alexandremuassab@gmail.com>
2023-03-17 17:30:50 -0300

    adicionado os modulos e aplicando lint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,51 +1,59 @@
-import Components from "unplugin-vue-components/vite";
-import { NaiveUiResolver } from "unplugin-vue-components/resolvers";
+import Components from 'unplugin-vue-components/vite';
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers';
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
-	modules: ["@vueuse/nuxt", "@vueuse/motion/nuxt", "@nuxtjs/tailwindcss"],
-	typescript: {
-		shim: false,
-	},
-	experimental: {
-		reactivityTransform: true,
-	},
-	build: {
-		transpile:
-			process.env.NODE_ENV === "production"
-				? [
-					"naive-ui",
-					"vueuc",
-					"@css-render/vue3-ssr",
-					"@juggle/resize-observer",
-				]
-				: ["@juggle/resize-observer"],
-	},
-	vite: {
-		optimizeDeps: {
-			include:
-				process.env.NODE_ENV === "development"
-					? ["naive-ui", "vueuc", "date-fns-tz/esm/formatInTimeZone"]
-					: [],
-		},
-		plugins: [
-			Components({
-				resolvers: [NaiveUiResolver()],
-			}),
-		],
-	},
-	imports: {
-		presets: [
-			{
-				from: "naive-ui",
-				imports: [
-					"useMessage",
-					"useNotification",
-					"useDialog",
-					"useLoadingBar",
-				],
-			},
-		],
-	},
+  modules: [
+    '@vueuse/nuxt',
+    '@vueuse/motion/nuxt',
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/eslint-module',
+    '@nuxt/devtools'
+  ],
+  typescript: {
+    shim: false,
+    strict: true
+  },
+  experimental: {
+    reactivityTransform: true
+  },
+  build: {
+    transpile:
+      process.env.NODE_ENV === 'production'
+        ? [
+          'naive-ui',
+          'vueuc',
+          '@css-render/vue3-ssr',
+          '@juggle/resize-observer'
+        ]
+        : ['@juggle/resize-observer']
+  },
+  vite: {
+    optimizeDeps: {
+      include:
+        process.env.NODE_ENV === 'development'
+          ? ['naive-ui', 'vueuc', 'date-fns-tz/esm/formatInTimeZone']
+          : []
+    },
+    plugins: [
+      Components({
+        resolvers: [NaiveUiResolver()]
+      })
+    ]
+  },
+  imports: {
+    presets: [
+      {
+        from: 'naive-ui',
+        imports: ['useMessage', 'useNotification', 'useDialog', 'useLoadingBar']
+      }
+    ]
+  },
+  eslint: {
+    emitError: false,
+    emitWarning: false,
+    fix: true,
+    fixTypes: ['problem']
+  }
 });
