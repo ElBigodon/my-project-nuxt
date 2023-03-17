7b88a389e6ff66e46e270ab193024add7728d0cd
ElBigodon <alexandremuassab@gmail.com>
2023-03-17 17:30:25 -0300

    configurando os linters

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,33 @@
-// eslint-disable-next-line no-undef
 module.exports = {
-	root: true,
-	env: {
-		browser: true,
-		es2022: true,
-	},
-	extends: [
-		"eslint:recommended",
-		"plugin:vue/vue3-recommended",
-		"plugin:@typescript-eslint/recommended",
-		"plugin:nuxt/recommended"
-	],
-	overrides: [],
-	parser: "vue-eslint-parser",
-	plugins: ["vue", "@typescript-eslint"],
-	rules: {
-		"no-undef": "off",
-		indent: ["error", "tab"],
-		"linebreak-style": ["error", "windows"],
-		quotes: ["error", "double"],
-		semi: ["error", "always"],
-		"block-spacing": ["error", "always"],
-		"vue/multi-word-component-names": [
-			"error", 
-			{
-				"ignores": ["index", "default"]
-			}
-		],
-		"vue/no-undef-properties": ["error"]
-	},
-	ignorePatterns: ["**.d.ts", "plugins/**/*.{js,ts}"]
+  root: true,
+  env: {
+    browser: true,
+    es2022: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    'plugin:@typescript-eslint/recommended',
+    '@nuxtjs/eslint-config-typescript'
+  ],
+  overrides: [],
+  parser: 'vue-eslint-parser',
+  plugins: ['vue', '@typescript-eslint'],
+  rules: {
+    'no-undef': 'off',
+    indent: ['error', 2, { ArrayExpression: 1 }],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'block-spacing': ['error', 'always'],
+    'vue/multi-word-component-names': [
+      'error',
+      {
+        ignores: ['index', 'default']
+      }
+    ],
+    'vue/no-undef-properties': ['error'],
+    'no-tabs': 'off'
+  },
+  ignorePatterns: ['**.d.ts', 'plugins/**/*.{js,ts}']
 };
